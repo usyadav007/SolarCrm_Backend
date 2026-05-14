e09f9133ff66bbe5ece5df0dea1d22a9d87b2967
umesh Yadav <usyadav2007@gmail.com>
2026-05-14 16:54:06 +0530

    CROSS issue fixed

--- server.js
+++ server.js
@@ -1,29 +1,50 @@
 require("dotenv").config();
 
+const cors = require("cors");
+
 const app = require("./src/app");
 const sequelize = require("./src/config/db");
-const { startReminderJob } = require("./src/services/reminderService");
 
-const { startAttendanceJob } = require("./src/services/attendanceService");
+const {
+  startReminderJob,
+} = require("./src/services/reminderService");
+
+const {
+  startAttendanceJob,
+} = require("./src/services/attendanceService");
+
+
+// ✅ CORS Setup
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 
 const startServer = async () => {
   try {
+
     await sequelize.authenticate();
     console.log("DB Connected ✅");
 
     await sequelize.sync();
     console.log("DB Synced ✅");
 
-    await sequelize.sync();
-
-    startReminderJob(); // 🔥 ADD THIS
+    startReminderJob();
 
     startAttendanceJob();
 
     app.listen(8000, () => {
-      console.log("Server running on 8000 🚀");
+      console.log(
+        "Server running on 8000 🚀"
+      );
     });
+
   } catch (error) {
+
     console.error(error);
   }
 };
