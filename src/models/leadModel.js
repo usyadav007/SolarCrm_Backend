edae9c83b74e9cc40a71dd1705459ad82c43b06c
umesh Yadav <usyadav2007@gmail.com>
2026-06-10 15:06:12 +0530

    correction in lead model

--- src/models/leadModel.js
+++ src/models/leadModel.js
@@ -1,25 +1,41 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  return sequelize.define("Lead", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
+
+  return sequelize.define(
+    "Lead",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      customer_name: DataTypes.STRING,
+
+      phone: DataTypes.STRING,
+
+      city: DataTypes.STRING,
+
+      source: DataTypes.STRING,
+
+      status: {
+        type: DataTypes.STRING,
+        defaultValue: "new",
+      },
+
+      assigned_to: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+
+      customer_id: DataTypes.INTEGER,
     },
-    customer_name: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    city: DataTypes.STRING,
-    source: DataTypes.STRING,
-    status: {
-      type: DataTypes.STRING,
-      defaultValue: "new",
-    },
-    assigned_to: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    customer_id: DataTypes.INTEGER,
-   
-  });
+    {
+      tableName: "Leads",   // IMPORTANT
+      freezeTableName: true,
+      timestamps: true,
+    }
+  );
+
 };
